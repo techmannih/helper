35d41ff08bb631f48f4b6353569a2373a2b66dd4
Raphael Costa <raphael@raphaelcosta.net>
2025-03-17 18:20:45 -0400

    feat: remove workflow tables (#75)
    - Follow-up from https://github.com/antiwork/helper/pull/57
    - Remove workflow related tables

---
+++ apps/nextjs/src/db/drizzle/0062_sturdy_william_stryker.sql
@@ -0,0 +1,6 @@
+DROP TABLE "workflows_workflow" CASCADE;--> statement-breakpoint
+DROP TABLE "workflows_workflowaction" CASCADE;--> statement-breakpoint
+DROP TABLE "workflows_workflowconditiongroup" CASCADE;--> statement-breakpoint
+DROP TABLE "workflows_workflowcondition" CASCADE;--> statement-breakpoint
+DROP TABLE "workflows_workflowrun" CASCADE;--> statement-breakpoint
+DROP TABLE "workflows_workflowrunaction" CASCADE;
